0ac773b0ca934cb223e732f67a8bf88da5c6060d
Samuel Schmidt <samuel@dersam.net>
2016-04-20 12:09:08 -0400

    Add viewConfigurableProduct action.

--- src/LinusShops/Contexts/Magento.php
+++ src/LinusShops/Contexts/Magento.php
@@ -1,6 +1,9 @@
 <?php
 /**
+ * Provides some Magento specific actions, with verification against the
+ * Magento DB.
  *
+ * This trait expects to be applied to a class that inherits from LinusShops\Contexts\Web.
  *
  * @author Sam Schmidt <samuel@dersam.net>
  * @since 2016-03-15
@@ -10,11 +13,23 @@ namespace LinusShops\Contexts;
 
 trait Magento
 {
-    /**
-     * @Given /^I should be able to add the product to my cart$/
-     */
-    public function assertCanAddProductToCart()
+    public function viewConfigurableProduct($productId, $autoselect = [])
     {
-        throw new \Behat\Behat\Tester\Exception\PendingException();
+        $product = \Mage::getModel('catalog/product')->load($productId);
+        $url = $product->getProductUrl();
+        $hash = '#';
+        foreach ($autoselect as $key => $value) {
+            if ($hash != '#') {
+                $hash .= '&';
+            }
+
+            $hash .= "{$key}={$value}";
+        }
+
+        $this->visit($url.$hash);
+
+        $this->waitForVisibleText($product->getName());
+
+        return $product;
     }
 }
